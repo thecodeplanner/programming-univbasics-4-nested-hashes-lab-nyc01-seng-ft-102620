c48c37dec21b56633d9d01034b12931fabe770ed
Melissa <thecodeplanner@gmail.com>
2020-10-13 01:54:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,8 @@ def changing_alan
       }
     }
   alans_new_info = "GUI"
-  alans_new_info = programmer_hash[:alan_kay][:known_for] 
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  return programmer_hash
 end
 
 
