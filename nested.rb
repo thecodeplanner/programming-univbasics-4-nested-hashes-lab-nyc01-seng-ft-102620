f0105fc67a75f772ed462ce90bdb6c8dca960120
Melissa <thecodeplanner@gmail.com>
2020-10-13 01:37:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,7 @@ def changing_alan
     }
   programmer_hash[:alan_kay][:known_for] = "GUI"
 end
-  programmer_hash
-end
+
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
