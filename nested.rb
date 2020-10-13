44689b486f4f0210808bada91b36a94dd3b00b4e
Melissa <thecodeplanner@gmail.com>
2020-10-13 01:15:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  new_hash = programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
-  programmer_hash
+  new_hash
 end
 
 def changing_alan
