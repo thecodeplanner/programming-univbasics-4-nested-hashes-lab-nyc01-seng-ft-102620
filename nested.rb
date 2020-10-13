5e60f283fdaba5edc8d52c120a1adde6391b6a0f
Melissa <thecodeplanner@gmail.com>
2020-10-13 01:19:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,8 @@ def adding_matz
     }
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
-
+  programmer_hash
+end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
